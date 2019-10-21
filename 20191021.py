a5430fd06da98f209ebad86e219f69c874512ecc
Macr0Nerd <macronerdart@gmail.com>
2019-10-21 15:13:28 -0500

    Started 20192021

---
+++ 20191021.py
@@ -0,0 +1,10 @@
+"""
+Challenge for the Week of 2019/10/21
+-------------------------------------
+Given input integer n, calculate the Fibonacci sequence up to index n, lists 0 indexes of course.
+"""
+
+x = int(input("Index: "))  # Not Counted
+a=[0,1]  # Counted
+while(len(a)<=x):z=a[-2];z+=a[-1];a.append(z)  # Counted
+print(z)  # Not Counted
