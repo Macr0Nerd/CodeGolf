746ad5c5f93aff33f65fb2c21b6be2a0da788e5b
Macr0Nerd <macrocode@yahoo.com>
2020-10-20 14:30:16 -0500

    194

--- 20201019.py
+++ 20201019.py
@@ -22,8 +22,8 @@ Deadline & Submission: Next Week Friday @ 6 PM CST, 2020/10/23. Submission throu
 f = open("20201019.csv").readlines()  # not counted
 n=[]
 for i in f:
- z=list(map(int,list(filter(None,i.strip().split(",")))));m=0;u=len(z)
- for j in range(0,u**2):
+ z=list(map(int,list(filter(None,i.strip().split(",")))));m,u=0,len(z)
+ for j in range(u**2):
   if u==1:break
   m=0if m==u-1else m
   if z[m]>z[m+1]:z.insert(m+1,z.pop(m))
